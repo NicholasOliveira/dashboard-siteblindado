44b76541518606437a3c7f875fc04a453ad9edaf
Nicholas Oliveira <nicholas@atomocom.com.br>
2020-11-04 18:48:20 -0300

    subindo build

---
+++ build/precache-manifest.c3b9ba4d6261a098d1cc0fa9d5660f51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5069cfe628e078d8542922b111298730",
+    "url": "/desafio_front/build/index.html"
+  },
+  {
+    "revision": "faa6cc3da73363197a14",
+    "url": "/desafio_front/build/static/js/2.08dc0a6b.chunk.js"
+  },
+  {
+    "revision": "7baac2e68c562348b516173f26c107b7",
+    "url": "/desafio_front/build/static/js/2.08dc0a6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "764a20ceb673134db167",
+    "url": "/desafio_front/build/static/js/main.9f341878.chunk.js"
+  },
+  {
+    "revision": "f0ecfeb855ab4979e4e0",
+    "url": "/desafio_front/build/static/js/runtime-main.59558ae4.js"
+  },
+  {
+    "revision": "45a482bc3a19d70b7285730edc74b260",
+    "url": "/desafio_front/build/static/media/back-portal-site.45a482bc.png"
+  }
+]);
